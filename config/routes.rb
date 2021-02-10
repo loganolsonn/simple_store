44ec9ee77cdd45d80fae47e0cd299c1a127c1439
Logan Olson <nolson93@academic.rrc.ca>
2021-02-10 14:50:00 -0600

    Updated product controller and product index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
+  resources :products, only: %i[index show]
+  root to: "products#index"
+  # get 'products/index'
+  # get 'products/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
